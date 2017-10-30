6ee6dd06af874caed8b30618e469eced99d1ff3b
Cat Janowitz and Holly Wilkalis <cat@>
2017-10-30 09:16:45 -0700

    follow homework example and build out address book using Contact constructor and appropriate jquery callbackdirection

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-$(document).ready(function() {
+//business logic
+function Contact(first, last, email) {
+  this.firstName = first;
+  this.lastName = last;
+  this.email = email;
+}
 
+
+//user interface logic
+$(document).ready(function() {
+  $("form#contact-info").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#first-name").val();
+    var inputtedLastName = $("input#last-name").val();
+    var inputtedEmail = $("input#email").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail);
+
+    $("ul#contact-list").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#first-name").val("");
+    $("input#last-name").val("");
+    $("input#email").val("");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first").text(newContact.firstName);
+      $(".last").text(newContact.lastName);
+      $(".emailaddr").text(newContact.email);
+    });
+
+  });
 
 
 });
